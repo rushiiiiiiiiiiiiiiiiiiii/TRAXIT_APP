230e3604ff97b867cbb9efcf42728fb64a90666c
Rushikesh Arote <142008272+rushiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-07-22 20:24:25 +0530

    Update Conn.js

--- Traxitserver/Conn.js
+++ Traxitserver/Conn.js
@@ -20,10 +20,10 @@
         require('dotenv').config();
 
         const pool = mysql.createPool({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            database: process.env.DB_NAME,
+            host: sg2nlmysql23plsk.secureserver.net,
+            user: traxituser,
+            password: Abcd@1234,
+            database: traxit,
         });
         // const pool = mysql.createPool({
         //     connectionLimit: 10,
